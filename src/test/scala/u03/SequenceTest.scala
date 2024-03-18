4ce17fd597e396f3ce56379e6fee527a0e183a5f
Alessandro Venturini <ales.ventus21@gmail.com>
2024-03-18 10:43:13 +0100

    deleted test cases to test myself on tdd

--- src/test/scala/u03/SequenceTest.scala
+++ src/test/scala/u03/SequenceTest.scala
@@ -22,28 +22,16 @@ class SequenceTest:
     assertEquals(Cons(20, Cons(30, Nil())), filter(l)(_ >= 20))
     assertEquals(Cons(10, Cons(30, Nil())), filter(l)(_ != 20))
   
-  @Test def testTake() =
+  @Test def testTake() = 
     assertEquals(Cons(10, Cons(20, Nil())), take(l)(2))
-    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), take(l)(3))
     assertEquals(Nil(), take(l)(0))
+    assertEquals(Cons(10, Cons(20, Cons(30, Nil()))), take(l)(5))
     assertEquals(Nil(), take(Nil())(2))
   
-  @Test def testZip() = 
-    val l2: Sequence[String] = Cons("10", Cons("20", Cons("30", Nil())))
-    assertEquals(Cons((10, "10"), Cons((20, "20"), Cons((30, "30"), Nil()))), zip(l, l2))
-    assertEquals(Nil(), zip(l, Nil()))
-    assertEquals(Nil(), zip(Nil(), l2))
-    assertEquals(Nil(), zip(Nil(), Nil()))
+  @Test def testZip(): Unit = ???
 
-  @Test def testConcat() =
-    val l2: Sequence[Int] = Cons(40, Cons(50, Nil()))
-    assertEquals(Cons(10, Cons(20, Cons(30, Cons(40, Cons(50, Nil()))))), concat(l, l2))
-    assertEquals(Cons(40, Cons(50, Nil())), concat(Nil(), l2))
-  @Test def testFlatMap() =
-    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), flatMap(l)(v => Cons(v + 1, Nil())))
-    assertEquals(Nil(), flatMap(Nil())(v => Cons(v, Nil())))
+  @Test def testConcat(): Unit = ???
 
-  @Test def testMin() =
-    assertEquals(Just(10), min(l))
-    assertEquals(Just(1), min(Cons(1, Nil())))
-    assertEquals(Empty(), min(Nil()))
+  @Test def testFlatMap(): Unit = ???
+
+  @Test def testMin(): Unit = ???
